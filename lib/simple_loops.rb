6489113022ccbad7075d17e37954e2c159d87021
Alyssa Lerner <alyssaplerner@gmail.com>
2020-07-17 16:13:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,24 @@
-def loop_message_five_times(string)
+def loop_message_five_times(message)
   counter = 0
   
   while counter < 5 do
-    puts string
+    puts message
   end
 end
+
+def loop_message_n_times(message, n)
+  counter = 0
+  
+  while counter < n do
+    puts message
+  end
+end
+
+def output_array(array)
+  counter = 0
+  
+  while array[counter] do
+    puts array[counter]
+  end
+end
+
